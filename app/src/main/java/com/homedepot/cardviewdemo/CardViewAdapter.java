17f5e59e97d7f1e28b5e1f33eafd52a682ad935b
Davis, Jermaine <jermaine_a_davis@homedepot.com>
2016-06-13 21:33:33 -0400

    Initial commit

---
+++ app/src/main/java/com/homedepot/cardviewdemo/CardViewAdapter.java
@@ -0,0 +1,47 @@
+package com.homedepot.cardviewdemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by associate on 5/18/16.
+ */
+public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHolder> {
+    private String[] mDataSet;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public CardViewAdapter(String[] myDataSet) {
+        mDataSet = myDataSet;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder((TextView) v);
+        return vh;
+    }
+}
